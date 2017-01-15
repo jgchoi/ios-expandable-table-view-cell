b15069d73b471ec77d04897eee56035f8f8bc450
John Choi <jchoi@north60tech.com>
2017-01-14 22:29:45 -0500

    alloc init

---
+++ ExpandableTableViewCellExample/ExpandableTableViewCellExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpandableTableViewCellExample
+//
+//  Created by Jung Geon Choi on 2017-01-14.
+//  Copyright © 2017 Seneca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
